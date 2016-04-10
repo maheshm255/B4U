98aa654680be6ed548e21e7ca4ded3ce6438d953
rahulrahul208 <rahul@Subrats-iMac.local>
2016-04-10 13:15:35 +0530

    Committed for COD Order Confirmed
    Committed for COD Order Confirmed

--- bro4u/UI/Thanks Screen/b4u_OrderConfirmedCODViewController.swift
+++ bro4u/UI/Thanks Screen/b4u_OrderConfirmedCODViewController.swift
@@ -44,10 +44,8 @@ class b4u_OrderConfirmedCODViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         self.addLoadingIndicator()
-//        currentOrder =  bro4u_DataManager.sharedInstance.orderData[0]
         
         self.createOrder()
-//        self.getDataOfThanksScreen()
 
     }
 
@@ -102,33 +100,70 @@ class b4u_OrderConfirmedCODViewController: UIViewController {
         downView.hidden = true
         btnContinue.hidden = true
         
+        
+
         var user_id = ""
-        var total_cost = "98"
-        var service_time = "12pm-2pm"
-        var service_date = "2-09-2015"
+        var total_cost = ""
+        var service_time = ""
+        var service_date = ""
         var selection = "[{%22option_112%22%3A%222%22%2C%22option_105%22%3A%221%22%2C%22user_id%22%3A%221626%22%2C%22item_id%22%3A%221928%22%2C%22vendor_id%22%3A%22132%22%2C%22unit_quantity%22%3A%221%22%2C%22grand_total%22%3A98%2C%22sub_total%22%3A98%2C%22delivery_charge%22%3A%220.00%22%2C%22deducted_from_wallet%22%3A0%2C%22deducted_using_coupon%22%3A0}]"
-        var grand_total="98.0"
-        var night_delivery_charge="0.00"
-        var customer_name="Harshal+Zope"
-        var vendor_id="132"
+        var grand_total=""
+        var night_delivery_charge=""
+        var customer_name=""
+        var vendor_id=""
         var custom_message="ihih"
-        var address_id="2"
-        var email="harshal.zope1990@gmail.com"
-        var mobile="8149881090"
-        var item_id="1928"
+        var address_id=""
+        var email=""
+        var mobile=""
+        var item_id=""
         var payment_wallet="0"
-        var coupon="bash200"
+        var coupon=""
         var imei="359296054612743"
-        var cat_id="13"
-        var latitude="23.344543"
-        var longitude="49878428"
+        var cat_id=""
+        var latitude=""
+        var longitude=""
 
         
         if let loginInfoData:b4u_LoginInfo = bro4u_DataManager.sharedInstance.loginInfo{
             
             user_id = loginInfoData.userId! //Need to use later
-            
         }
+
+        total_cost =  bro4u_DataManager.sharedInstance.selectedSuggestedPatner!.custPrice!
+        
+        if let couponCode =    bro4u_DataManager.sharedInstance.copiedCopunCode
+        {
+            coupon = couponCode
+        }
+        service_time =   bro4u_DataManager.sharedInstance.selectedTimeSlot!
+        service_date =   "\(bro4u_DataManager.sharedInstance.selectedDate!)"
+
+        if let selectionLocal: b4u_SelectionModel =    bro4u_DataManager.sharedInstance.selectionData[0]{
+            //Need to Implement
+            grand_total = "\(selectionLocal.grandTotal)"
+            night_delivery_charge = selectionLocal.nightCharge!
+            item_id = selectionLocal.itemId!
+            vendor_id = selectionLocal.vendorId!
+            selection =  ""
+        }
+        
+        if let catIDData:b4u_OrderDetailModel = bro4u_DataManager.sharedInstance.orderDetailData[0]{
+
+            cat_id = catIDData.catId! //Need to use later
+        }
+
+
+        if let addressData:b4u_AddressDetails = bro4u_DataManager.sharedInstance.address[0]{
+
+            address_id = "\(addressData.addressId!)" //Need to use later
+            email = addressData.email! //Need to use later
+            latitude = addressData.lattitude!
+            longitude = addressData.longitude!
+            mobile = addressData.phoneNumber!
+            customer_name = addressData.name!
+        }
+
+
 //
 //        if let catIDData:b4u_Category = bro4u_DataManager.sharedInstance.categoryAndSubOptions[0]{
 //            
@@ -246,7 +281,7 @@ class b4u_OrderConfirmedCODViewController: UIViewController {
         {
             self.lblServiceStatus.text = orderStatus
         }
-        if let price = confirmedOrder!.finalTotal //Need to check Key
+        if let price = confirmedOrder!.totalCost //Need to check Key
         {
             self.lblAmount.text = "Rs. \(price).00"
         }
