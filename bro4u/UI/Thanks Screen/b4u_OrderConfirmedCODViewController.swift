d0c96f7b437b59bd6475fe2e9d4b3556474ea17a
rahulrahul208 <cognizantimac6@MSP-User3.local>
2016-04-11 11:45:09 +0530

    Modified for COD Payment
    Modified for COD Payment

--- bro4u/UI/Thanks Screen/b4u_OrderConfirmedCODViewController.swift
+++ bro4u/UI/Thanks Screen/b4u_OrderConfirmedCODViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreLocation
 
-class b4u_OrderConfirmedCODViewController: UIViewController {
+class b4u_OrderConfirmedCODViewController: UIViewController  , createOrderDelegate{
 
     var order_id:String?
     var confirmedOrder:b4u_OrdersModel?
@@ -44,9 +44,18 @@ class b4u_OrderConfirmedCODViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         self.addLoadingIndicator()
-        
-        self.createOrder()
-        
+      
+        b4u_Utility.sharedInstance.activityIndicator.startAnimating()
+        let createOrderObj = b4u_CreateOrder()
+        createOrderObj.delegate = self
+        createOrderObj.createOrder()
+        topView.hidden = true
+        middleView.hidden = true
+        downView.hidden = true
+        btnContinue.hidden = true
+
+//        self.createOrder()
+      
         lblServiceStatus.layer.borderWidth = 1.0
         lblServiceStatus.layer.borderColor = UIColor.lightGrayColor().CGColor
 
@@ -69,171 +78,147 @@ class b4u_OrderConfirmedCODViewController: UIViewController {
     }
     */
     
-    func createOrder()
-    {
-        
-        /*
-        /index.php/order/place_cod_order?
-        user_id=1626&
-        total_cost=98&
-        service_time=12pm-2pm&
-        service_date=2-09-2015&
-        selection=[{%22option_112%22%3A%222%22%2C%22option_105%22%3A%221%22%2C%22user_id%22%3A%221626%22%2C%22item_id%22%3A%221928%22%2C%22vendor_id%22%3A%22132%22%2C%22unit_quantity%22%3A%221%22%2C%22grand_total%22%3A98%2C%22sub_total%22%3A98%2C%22delivery_charge%22%3A%220.00%22%2C%22deducted_from_wallet%22%3A0%2C%22deducted_using_coupon%22%3A0}]&
-        grand_total=98.0&
-        night_delivery_charge=0.00&
-        customer_name=Harshal+Zope&
-        vendor_id=132&
-        custom_message=ihih&
-        address_id=2&
-        email=harshal.zope1990%40gmail.com&
-        mobile=8149881090&
-        item_id=1928&
-        payment_wallet=0&
-        coupon=bash200&
-        imei=359296054612743&
-        cat_id=13&
-        latitude=23.344543&
-        longitude=49878428
-        
-        
-        */
-        
-        b4u_Utility.sharedInstance.activityIndicator.startAnimating()
-        topView.hidden = true
-        middleView.hidden = true
-        downView.hidden = true
-        btnContinue.hidden = true
-        
-        
-
-        var user_id = ""
-        var total_cost = ""
-        var service_time = ""
-        var service_date = ""
-        var selection = "[{%22option_112%22%3A%222%22%2C%22option_105%22%3A%221%22%2C%22user_id%22%3A%221626%22%2C%22item_id%22%3A%221928%22%2C%22vendor_id%22%3A%22132%22%2C%22unit_quantity%22%3A%221%22%2C%22grand_total%22%3A98%2C%22sub_total%22%3A98%2C%22delivery_charge%22%3A%220.00%22%2C%22deducted_from_wallet%22%3A0%2C%22deducted_using_coupon%22%3A0}]"
-        var grand_total=""
-        var night_delivery_charge=""
-        var customer_name=""
-        var vendor_id=""
-        var custom_message="ihih"
-        var address_id="1"
-        var email=""
-        var mobile=""
-        var item_id=""
-        var payment_wallet="0"
-        var coupon=""
-        var imei="359296054612743"
-        var cat_id=""
-        var latitude=""
-        var longitude=""
-
-        
-        if let loginInfoData:b4u_LoginInfo = bro4u_DataManager.sharedInstance.loginInfo{
-            
-            user_id = loginInfoData.userId! //Need to use later
-        }
-
-        total_cost =  bro4u_DataManager.sharedInstance.selectedSuggestedPatner!.custPrice!
-        
-        if let couponCode =    bro4u_DataManager.sharedInstance.copiedCopunCode
-        {
-            coupon = couponCode
-        }
-        service_time =   bro4u_DataManager.sharedInstance.selectedTimeSlot!
-        service_date =   "\(bro4u_DataManager.sharedInstance.selectedDate!)"
-
-        
-        if let orderDetailModel = bro4u_DataManager.sharedInstance.orderDetailData.first
-        {
-            if let selectionLocal: b4u_SelectionModel =  orderDetailModel.selection?.first{
-                //Need to Implement
-                grand_total = "\(selectionLocal.grandTotal)"
-                
-                if let deliverChages = selectionLocal.nightCharge
-                {
-                    night_delivery_charge = deliverChages
-                }
-                item_id = selectionLocal.itemId!
-                vendor_id = selectionLocal.vendorId!
-                selection =  ""
-            }
-            
-        }
-    
-        if let catIDData:b4u_OrderDetailModel = bro4u_DataManager.sharedInstance.orderDetailData[0]{
-
-            cat_id = catIDData.catId! //Need to use later
-        }
-
-
-        if let addressData:b4u_AddressDetails = bro4u_DataManager.sharedInstance.address[0]{
-
-            if let addressId = addressData.addressId
-            {
-                address_id = "\(addressId)" //Need to use later
-            }
-            
-            if let aEmail = addressData.email
-            {
-                email = aEmail //Need to use later
-            }
-            if let lat = addressData.lattitude
-            {
-                latitude = lat
-            }
-            
-            if let long = addressData.longitude
-            {
-                longitude = long
-            }
-            
-            if let phoneNumber = addressData.phoneNumber
-            {
-                mobile = phoneNumber
-            }
-            if let cusName = addressData.name
-            {
-                customer_name = cusName
-            }
-        }
-
-
+//    func createOrder()
+//    {
+//        
+//        /*
+//        /index.php/order/place_cod_order?
+//        user_id=1626&
+//        total_cost=98&
+//        service_time=12pm-2pm&
+//        service_date=2-09-2015&
+//        selection=[{%22option_112%22%3A%222%22%2C%22option_105%22%3A%221%22%2C%22user_id%22%3A%221626%22%2C%22item_id%22%3A%221928%22%2C%22vendor_id%22%3A%22132%22%2C%22unit_quantity%22%3A%221%22%2C%22grand_total%22%3A98%2C%22sub_total%22%3A98%2C%22delivery_charge%22%3A%220.00%22%2C%22deducted_from_wallet%22%3A0%2C%22deducted_using_coupon%22%3A0}]&
+//        grand_total=98.0&
+//        night_delivery_charge=0.00&
+//        customer_name=Harshal+Zope&
+//        vendor_id=132&
+//        custom_message=ihih&
+//        address_id=2&
+//        email=harshal.zope1990%40gmail.com&
+//        mobile=8149881090&
+//        item_id=1928&
+//        payment_wallet=0&
+//        coupon=bash200&
+//        imei=359296054612743&
+//        cat_id=13&
+//        latitude=23.344543&
+//        longitude=49878428
+//        
+//        
+//        */
+//        
+//        b4u_Utility.sharedInstance.activityIndicator.startAnimating()
+//        topView.hidden = true
+//        middleView.hidden = true
+//        downView.hidden = true
+//        btnContinue.hidden = true
+//        
+//        
 //
-//        if let catIDData:b4u_Category = bro4u_DataManager.sharedInstance.categoryAndSubOptions[0]{
-//            
-//            cat_id = catIDData.catId! //Need to use later
-//        }
+//        var user_id = ""
+//        var total_cost = ""
+//        var service_time = ""
+//        var service_date = ""
+//        var selection = "[{%22option_112%22%3A%222%22%2C%22option_105%22%3A%221%22%2C%22user_id%22%3A%221626%22%2C%22item_id%22%3A%221928%22%2C%22vendor_id%22%3A%22132%22%2C%22unit_quantity%22%3A%221%22%2C%22grand_total%22%3A98%2C%22sub_total%22%3A98%2C%22delivery_charge%22%3A%220.00%22%2C%22deducted_from_wallet%22%3A0%2C%22deducted_using_coupon%22%3A0}]"
+//        var grand_total=""
+//        var night_delivery_charge=""
+//        var customer_name=""
+//        var vendor_id=""
+//        var custom_message="ihih"
+//        var address_id="1"
+//        var email=""
+//        var mobile=""
+//        var item_id=""
+//        var payment_wallet="0"
+//        var coupon=""
+//        var imei="359296054612743"
+//        var cat_id=""
+//        var latitude=""
+//        var longitude=""
+//
 //        
-//        if let currentLocationData:CLLocation = bro4u_DataManager.sharedInstance.currenLocation{
+//        if let loginInfoData:b4u_LoginInfo = bro4u_DataManager.sharedInstance.loginInfo{
 //            
-//            latitude = "\(currentLocationData.coordinate.latitude)"
-//            longitude = "\(currentLocationData.coordinate.longitude)"
+//            user_id = loginInfoData.userId! //Need to use later
 //        }
+//
+//        total_cost =  bro4u_DataManager.sharedInstance.selectedSuggestedPatner!.custPrice!
 //        
+//        if let couponCode =    bro4u_DataManager.sharedInstance.copiedCopunCode
+//        {
+//            coupon = couponCode
+//        }
+//        service_time =   bro4u_DataManager.sharedInstance.selectedTimeSlot!
+//        service_date =   "\(bro4u_DataManager.sharedInstance.selectedDate!)"
+//
+//        
+//        if let orderDetailModel = bro4u_DataManager.sharedInstance.orderDetailData.first
+//        {
+//            if let selectionLocal: b4u_SelectionModel =  orderDetailModel.selection?.first{
+//
+//              grand_total = "\(selectionLocal.grandTotal)"
+//                
+//                if let deliverChages = selectionLocal.nightCharge
+//                {
+//                    night_delivery_charge = deliverChages
+//                }
+//                item_id = selectionLocal.itemId!
+//                vendor_id = selectionLocal.vendorId!
+//                selection =  ""
+//            }
+//            
+//        }
+//    
+//        if let catIDData:b4u_OrderDetailModel = bro4u_DataManager.sharedInstance.orderDetailData[0]{
+//
+//            cat_id = catIDData.catId!
+//        }
+//
+//
 //        if let addressData:b4u_AddressDetails = bro4u_DataManager.sharedInstance.address[0]{
+//
+//            if let addressId = addressData.addressId
+//            {
+//                address_id = "\(addressId)"
+//            }
 //            
-//            address_id = "\(addressData.addressId!)" //Need to use later
-//            email = "\(addressData.email!)" //Need to use later
-//            latitude = "\(addressData.lattitude)"
-//            longitude = "\(addressData.longitude)"
-//            mobile = "\(addressData.phoneNumber)"
-//            customer_name = "\(addressData.name)"
+//            if let aEmail = addressData.email
+//            {
+//                email = aEmail
+//            }
+//            if let lat = addressData.lattitude
+//            {
+//                latitude = lat
+//            }
 //            
+//            if let long = addressData.longitude
+//            {
+//                longitude = long
+//            }
+//            
+//            if let phoneNumber = addressData.phoneNumber
+//            {
+//                mobile = phoneNumber
+//            }
+//            if let cusName = addressData.name
+//            {
+//                customer_name = cusName
+//            }
 //        }
-        
-        
-        
-        let params = "?user_id=\(user_id)&total_cost=\(total_cost)&service_time=\(service_time)&service_date=\(service_date)&selection=\(selection)&grand_total=\(grand_total)&night_delivery_charge=\(night_delivery_charge)&customer_name=\(customer_name)&vendor_id=\(vendor_id)&custom_message=\(custom_message)&address_id=\(address_id)&email=\(email)&mobile=\(mobile)&item_id=\(item_id)&payment_wallet=\(payment_wallet)&coupon=\(coupon)&imei=\(imei)&cat_id=\(cat_id)&latitude=\(latitude)&longitude=\(longitude)"
-        
-        b4u_WebApiCallManager.sharedInstance.getApiCall(kPlaceCashOnDeliveryIndex , params:params, result:{(resultObject) -> Void in
-            
-            print(" COD Order Data Received")
-            
-            self.getDataOfThanksScreen(resultObject as! String)
-            print(resultObject)
-        })
-    }
-    
+//
+//      
+//        
+//        let params = "?user_id=\(user_id)&total_cost=\(total_cost)&service_time=\(service_time)&service_date=\(service_date)&selection=\(selection)&grand_total=\(grand_total)&night_delivery_charge=\(night_delivery_charge)&customer_name=\(customer_name)&vendor_id=\(vendor_id)&custom_message=\(custom_message)&address_id=\(address_id)&email=\(email)&mobile=\(mobile)&item_id=\(item_id)&payment_wallet=\(payment_wallet)&coupon=\(coupon)&imei=\(imei)&cat_id=\(cat_id)&latitude=\(latitude)&longitude=\(longitude)"
+//        
+//        b4u_WebApiCallManager.sharedInstance.getApiCall(kPlaceCashOnDeliveryIndex , params:params, result:{(resultObject) -> Void in
+//            
+//            print(" COD Order Data Received")
+//            
+//            self.getDataOfThanksScreen(resultObject as! String)
+//            print(resultObject)
+//        })
+//    }
+  
     func getDataOfThanksScreen(result:String)
     {
         if result  == "Success"
@@ -244,17 +229,15 @@ class b4u_OrderConfirmedCODViewController: UIViewController {
 
             if let loginInfoData:b4u_LoginInfo = bro4u_DataManager.sharedInstance.loginInfo{
                 
-                user_id = loginInfoData.userId! //Need to use later
+                user_id = loginInfoData.userId!
                 
             }
             if let orderID = bro4u_DataManager.sharedInstance.orderId{
                 
-                order_id = "\(orderID)" //Need to use later
+                order_id = "\(orderID)"
                 
             }
 
-            
-            //user_id = "15"
             let params = "?order_id=\(order_id)&user_id=\(user_id)"
             b4u_WebApiCallManager.sharedInstance.getApiCall(kOrderConfirmedIndex , params:params, result:{(resultObject) -> Void in
                 
@@ -346,5 +329,12 @@ class b4u_OrderConfirmedCODViewController: UIViewController {
         self.view.bringSubviewToFront(b4u_Utility.sharedInstance.activityIndicator)
         b4u_Utility.sharedInstance.activityIndicator.center = self.view.center
     }
+  
+  
+  func hasOrderCreated(resultObject:String)
+  {
+    
+    self.getDataOfThanksScreen(resultObject)
+  }
 
 }
