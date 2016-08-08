176e0a416ae9173959b78c001c9f9968b0627d80
rahulrahul208 <cognizantimac6@MSP-User3.local>
2016-08-08 16:19:26 +0530

    Modified for Bro4u
    Modified for Bro4u

--- Pods/Target Support Files/Pods-bro4u/Pods-bro4u-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_bro4u : NSObject
-@end
-@implementation PodsDummy_Pods_bro4u
-@end
--- Pods/Target Support Files/Pods-bro4u/Pods-bro4u-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_bro4uVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_bro4uVersionString[];
-
